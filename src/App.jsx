56e6fb84374b12960ebf331332032b0d49c2aa0b
Amirhossein Payab <amirpayab2002@gmail.com>
2026-02-24 14:23:46 +0330

    smart counter with out reloading page be ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count,setCount] = useState(0)
+
+  function increase(){
+   setCount(count + 1)
+  }
+
+  const decrease = () =>{
+    setCount(count - 1)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Smart Counter</h1>
+      <h2>count : {count}</h2>
+      { count > 10 && <p>Too High 🚀</p> }
+      { count < 0 && <p style={{color:"red"}}>Negative 😬</p> }
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+
+    </div>
   )
 }
 
